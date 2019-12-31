e31493bb10b15870b0a3120ed5f360235b04025c
Roy Bailey <roybaileybiz@gmail.com>
2019-12-31 21:41:15 +0000

    added files from old bitbucket repo

---
+++ bin/versions.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+echo
+echo ---------- software installed ----------
+echo nvm `nvm --version`
+echo npm `npm --version`
+echo node `node --version`
+echo `python3 --version`
+echo `brew --version`
+echo `git --version`
+echo `java -version`
+echo
