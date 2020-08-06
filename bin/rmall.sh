0b7954f222cb38b287c46549b6a102b0cc1e12b9
Roy Bailey <roybaileybiz@gmail.com>
2020-08-06 10:52:27 +0100

    updated menu options

--- bin/rmall.sh
+++ bin/rmall.sh
@@ -7,16 +7,17 @@ function removeAll() {
   find . -name "$1" -depth -exec rm -rf {} \;
 }
 
-options=(idea target node build .DS_Store $1 exit)
+echo "This utility will force delete all folders/files in pwd and all sub-folders"
+echo "the following menu provides some common types for quick selection, or provide your own as an argument"
+echo "once seleted the folders/files to be removed will first be listed before a confirmation key to actually force remove them"
+echo
+
+# needs a really long option to force vertical display of menu
+options=($1 "'target' folders" "'node_modules' folders" "'build' folders" "idea related files and folders" "'.git' folders" ".DS_Store mac files" "---------- exit this script ----------")
 select menu in "${options[@]}";
 do
   echo -e "\nyou picked $menu ($REPLY)"
   case $menu in
-  idea)
-    echo -e "removing all .idea folders and *.iml files"
-    removeAll "*.iml"
-    removeAll ".idea"
-    ;;
   target)
     echo -e "removing all target folders"
     removeAll "target"
@@ -28,6 +29,15 @@ do
   build)
     echo -e "removing all build folders"
     removeAll "build"
+    ;;
+  idea)
+    echo -e "removing all .idea folders and *.iml files"
+    removeAll "*.iml"
+    removeAll ".idea"
+    ;;
+  .git)
+    echo -e "removing all .git files"
+    removeAll ".git"
     ;;
   .DS_Store)
     echo -e "removing all .DS_Store files"
